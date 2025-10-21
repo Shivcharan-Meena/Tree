78c520066a803140f5be81c7c3918925204f3532
Shivcharan-Meena <149512500+Shivcharan-Meena@users.noreply.github.com>
2025-10-22 01:35:26 +0530

    Create maxSumNonAdjecentNodes.cpp

---
+++ maxSumNonAdjecentNodes.cpp
@@ -0,0 +1,72 @@
+//https://www.geeksforgeeks.org/problems/maximum-sum-of-non-adjacent-nodes/1
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node*left;
+    Node*right;
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node*build(Node*root){
+    cout<<"Enter data ";
+    int n;
+    cin>>n;
+    if(n==-1){
+        return NULL;
+    }
+    if(root==NULL){
+        root=new Node(n);
+    }
+
+    cout<<"Enter data to the left of "<<root->data<<endl;
+    root->left=build(root->left);
+    cout<<"Enter data to the right of "<<root->data<<endl;
+    root->right=build(root->right);
+    return root;
+}
+
+
+
+pair<int,int> solve(Node* root) {
+        //base case
+        if(root == NULL) {
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
+        
+        pair<int,int> res;
+        
+        res.first = root->data + left.second + right.second;
+        
+        res.second = max(left.first, left.second) + max(right.first, right.second);
+        
+        return res;
+        
+    }
+    int getMaxSum(Node *root) 
+    {
+        pair<int,int> ans = solve(root);
+        return max(ans.first, ans.second);
+    }
+
+int main(){
+    Node*root=NULL;
+     // 1 2 4 -1 -1 3 -1 -1 5 6 -1 -1 -1 Balanced Tree
+    
+    root=build(root);
+  cout<<getMaxSum(root)<<endl;
+   return 0;
+}
+  
